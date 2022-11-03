139845dcb10cf90462eadd3ebc7f9b392e2906e1
acarrion36 <adriancarrion_36@hotmail.com>
2022-11-03 19:57:24 +0100

    "4"

--- api/api_oferta.php
+++ api/api_oferta.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin:localhost');
+header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header("Content-Type: application/json; charset=utf-8");
