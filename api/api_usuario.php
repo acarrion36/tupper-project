a3475b2329103eefce4085a2f8ad7b25e0653e70
acarrion36 <adriancarrion_36@hotmail.com>
2022-11-03 19:02:42 +0100

    "changes"

--- api/api_usuario.php
+++ api/api_usuario.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header("Content-Type: application/json; charset=utf-8");
 include_once("../class/class_usuario.php"); //incluimos clase producto
 include_once("../servicio.php"); 
