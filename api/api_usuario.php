ee8bdd4382d03733647bd8752c7ec04fd2423a0c
acarrion36 <adriancarrion_36@hotmail.com>
2022-11-03 17:10:05 +0100

    "nuevos cambios"

--- api/api_usuario.php
+++ api/api_usuario.php
@@ -1,12 +1,9 @@
 <?php
-
 header("Content-Type: application/json");
 include_once("../class/class_usuario.php"); //incluimos clase producto
 include_once("../servicio.php"); 
 login();
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
 $_POST=json_decode(file_get_contents('php://input'),true); //decodificar el json
 
 
--- servicio.php
+++ servicio.php
@@ -1,5 +1,7 @@
 <?php
-
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 include_once 'libr/nusoap.php'; //incluyendo al proyecto a la libreria nusoap.
 include_once 'class/conexion.php';//crear objeto de servicio
 
