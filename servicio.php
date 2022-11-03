b87396313d82eac1290136ade452993cc3ce7d5c
acarrion36 <adriancarrion_36@hotmail.com>
2022-11-03 17:36:06 +0100

    "mod 3"

--- servicio.php
+++ servicio.php
@@ -1,7 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
 include_once 'libr/nusoap.php'; //incluyendo al proyecto a la libreria nusoap.
 include_once 'class/conexion.php';//crear objeto de servicio
 
@@ -21,6 +19,9 @@ function login(){
     }
     if($_SERVER['PHP_AUTH_USER'] == 'elika_waste' && $_SERVER['PHP_AUTH_PW'] == 'elika123'){
         header('Content-Type: application/soap+json; charset=utf-8');
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
         return true;
     }else{
         header('WWW-Authenticate: Basic reaml="MiSoap"');
