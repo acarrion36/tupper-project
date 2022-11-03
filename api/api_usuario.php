5a4f4e29ceac682c1b050fbeeb7192a5a849c81d
acarrion36 <adriancarrion_36@hotmail.com>
2022-11-03 18:02:37 +0100

    "sin com"

--- api/api_usuario.php
+++ api/api_usuario.php
@@ -1,5 +1,8 @@
 <?php
-header("Content-Type: application/json");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header("Content-Type: application/json; charset=utf-8");
 include_once("../class/class_usuario.php"); //incluimos clase producto
 include_once("../servicio.php"); 
 login();
