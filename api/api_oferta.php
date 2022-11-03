10318ec19e6b3049056f15af37adfddc4386ae69
acarrion36 <adriancarrion_36@hotmail.com>
2022-11-03 15:25:41 +0100

    "CORS ERROR Solution"

--- api/api_oferta.php
+++ api/api_oferta.php
@@ -3,7 +3,9 @@ header("Content-Type: application/json");
 include_once("../class/class_oferta.php"); //incluimos clase producto
 include_once("../servicio.php"); 
 login();
-
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 $_POST=json_decode(file_get_contents('php://input'),true); //decodificar el json
 
 
