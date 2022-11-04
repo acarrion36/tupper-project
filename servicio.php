f226b436d9bb9b6bae24fc55211f4770b34cbc8b
acarrion36 <adriancarrion_36@hotmail.com>
2022-11-04 13:30:52 +0100

    "2"

--- servicio.php
+++ servicio.php
@@ -14,11 +14,9 @@ function login(){
   
     if (!isset($_SERVER['PHP_AUTH_USER'])){
         header('WWW-Authenticate: Basic reaml="MiSoap"');
-        header("HTTP/1.0 200 OK");
         exit;
     }
     if($_SERVER['PHP_AUTH_USER'] == 'elika_waste' && $_SERVER['PHP_AUTH_PW'] == 'elika123'){
-            // send some CORS headers so the API can be called from anywhere
         return true;
     }else{
         header('WWW-Authenticate: Basic reaml="MiSoap"');
