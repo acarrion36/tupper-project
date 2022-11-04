3e66c4ca49de80f25439f069ebedec01aecee49c
acarrion36 <adriancarrion_36@hotmail.com>
2022-11-04 13:38:19 +0100

    "2"

--- api/api_usuario.php
+++ api/api_usuario.php
@@ -1,11 +1,11 @@
 <?php
 
-            // send some CORS headers so the API can be called from anywhere
-            header("Access-Control-Allow-Origin: *");
-            header("Content-Type: application/json; charset=UTF-8");
-            header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
-            header("Access-Control-Max-Age: 3600");
-            header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+// send some CORS headers so the API can be called from anywhere
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
       
 
 include_once("../class/class_usuario.php"); //incluimos clase producto
