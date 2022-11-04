e0ebc284b25365c5c5e758678b999b03caf96d79
acarrion36 <adriancarrion_36@hotmail.com>
2022-11-04 13:13:10 +0100

    "ok"

--- servicio.php
+++ servicio.php
@@ -20,13 +20,6 @@ function login(){
         exit;
     }
     if($_SERVER['PHP_AUTH_USER'] == 'elika_waste' && $_SERVER['PHP_AUTH_PW'] == 'elika123'){
-            // send some CORS headers so the API can be called from anywhere
-        header("Access-Control-Allow-Origin: *");
-        header("Content-Type: application/json; charset=UTF-8");
-        header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
-        header("Access-Control-Max-Age: 3600");
-        header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-        header("HTTP/1.0 200 OK");
         return true;
     }else{
         header('WWW-Authenticate: Basic reaml="MiSoap"');
