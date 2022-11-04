52f047f48e653ad08089b18b7d3f6a731b023035
acarrion36 <adriancarrion_36@hotmail.com>
2022-11-04 13:27:18 +0100

    "2"

--- servicio.php
+++ servicio.php
@@ -12,12 +12,11 @@ $servicio->schemaTargetNamespace=$nombreespacio; //almacen el espacionombre de d
 
 function login(){
   
-   /* 
-   if (!isset($_SERVER['PHP_AUTH_USER'])){
+    if (!isset($_SERVER['PHP_AUTH_USER'])){
         header('WWW-Authenticate: Basic reaml="MiSoap"');
-        header('HTTP/1.0 401 Unautorized');
+        header("HTTP/1.0 200 OK");
         exit;
-    }*/
+    }
     if($_SERVER['PHP_AUTH_USER'] == 'elika_waste' && $_SERVER['PHP_AUTH_PW'] == 'elika123'){
             // send some CORS headers so the API can be called from anywhere
         return true;
