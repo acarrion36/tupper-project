c265b805ece72cf7a60ad7adef81907b9100e755
acarrion36 <adriancarrion_36@hotmail.com>
2022-11-04 12:45:50 +0100

    "ok"

--- servicio.php
+++ servicio.php
@@ -10,7 +10,7 @@ header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-//login();
+login();
 
 $servicio= new soap_server();
 $nombreespacio="urn:miserviciowsdl";
