102d07e8e3ad934d157d4644ea4cd74c6a45ee24
acarrion36 <adriancarrion_36@hotmail.com>
2022-11-04 13:32:12 +0100

    "2"

--- servicio.php
+++ servicio.php
@@ -14,6 +14,7 @@ function login(){
   
     if (!isset($_SERVER['PHP_AUTH_USER'])){
         header('WWW-Authenticate: Basic reaml="MiSoap"');
+        header("HTTP/1.0 200 OK");
         exit;
     }
     if($_SERVER['PHP_AUTH_USER'] == 'elika_waste' && $_SERVER['PHP_AUTH_PW'] == 'elika123'){
