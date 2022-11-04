c426568024051411d849adf059cd68007c0c33d5
acarrion36 <adriancarrion_36@hotmail.com>
2022-11-04 13:17:02 +0100

    "2"

--- servicio.php
+++ servicio.php
@@ -1,5 +1,4 @@
 <?php
-
 include_once 'libr/nusoap.php'; //incluyendo al proyecto a la libreria nusoap.
 include_once 'class/conexion.php';//crear objeto de servicio
 
@@ -24,7 +23,7 @@ function login(){
         return true;
     }else{
         header('WWW-Authenticate: Basic reaml="MiSoap"');
-        header("HTTP/1.0 200 OK");
+        header('HTTP/1.0 401 Unautorized');
         exit;
     }
 }
