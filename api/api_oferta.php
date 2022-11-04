f645bf2de13a2de6f52bda01277a3ca372482ddd
acarrion36 <adriancarrion_36@hotmail.com>
2022-11-04 13:58:34 +0100

    "3"

--- api/api_oferta.php
+++ api/api_oferta.php
@@ -1,5 +1,10 @@
 <?php
-header("HTTP/1.0 200 OK");
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 include_once("../class/class_oferta.php"); //incluimos clase producto
 include_once("../servicio.php"); 
 login();
