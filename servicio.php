9388b53debc3e8394b093867c23b5c585da43bfe
acarrion36 <adriancarrion_36@hotmail.com>
2022-11-04 13:32:37 +0100

    "2"

--- servicio.php
+++ servicio.php
@@ -12,11 +12,7 @@ $servicio->schemaTargetNamespace=$nombreespacio; //almacen el espacionombre de d
 
 function login(){
   
-    if (!isset($_SERVER['PHP_AUTH_USER'])){
-        header('WWW-Authenticate: Basic reaml="MiSoap"');
-        header("HTTP/1.0 200 OK");
-        exit;
-    }
+
     if($_SERVER['PHP_AUTH_USER'] == 'elika_waste' && $_SERVER['PHP_AUTH_PW'] == 'elika123'){
         return true;
     }else{
