4579bf3102d6d6d17afe5c50f355e6139859ac18
acarrion36 <adriancarrion_36@hotmail.com>
2022-11-04 13:36:40 +0100

    "2"

--- api/api_usuario.php
+++ api/api_usuario.php
@@ -6,8 +6,8 @@
             header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
             header("Access-Control-Max-Age: 3600");
             header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-            header("HTTP/1.0 200 OK");
-            
+      
+
 include_once("../class/class_usuario.php"); //incluimos clase producto
 include_once("../servicio.php"); 
 login();
