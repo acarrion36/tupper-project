1f19006a01787800d09c587079632a48d2e31641
a.carrion <101635337+acarrion36@users.noreply.github.com>
2022-11-09 15:48:55 +0100

    1

--- api/api_oferta.php
+++ api/api_oferta.php
@@ -9,7 +9,7 @@ include_once("../class/class_oferta.php"); //incluimos clase producto
 include_once("../servicio.php"); 
 login();
 
-$_POST=json_decode(file_get_contents('php://input'),true); //decodificar el jsonnn
+$_POST=json_decode(file_get_contents('php://input'),true); //decodificar el json
 
 
 switch($_SERVER['REQUEST_METHOD']){
