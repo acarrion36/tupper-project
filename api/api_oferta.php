97d4eee37aaea490461162c00af2ef7a564df229
a.carrion <101635337+acarrion36@users.noreply.github.com>
2022-11-09 15:46:41 +0100

    On branch pruebas

--- api/api_oferta.php
+++ api/api_oferta.php
@@ -9,7 +9,7 @@ include_once("../class/class_oferta.php"); //incluimos clase producto
 include_once("../servicio.php"); 
 login();
 
-$_POST=json_decode(file_get_contents('php://input'),true); //decodificar el jsonn
+$_POST=json_decode(file_get_contents('php://input'),true); //decodificar el jsonnn
 
 
 switch($_SERVER['REQUEST_METHOD']){
