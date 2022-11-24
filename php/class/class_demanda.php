a0287ff234c4208395f5aca49f5fda53c7e64163
a.carrion <101635337+acarrion36@users.noreply.github.com>
2022-11-24 12:15:40 +0100

    "fix2"

--- php/class/class_demanda.php
+++ php/class/class_demanda.php
@@ -31,7 +31,7 @@ class demanda{
     public static function guardarDemanda($id_usuario,$id_oferta,$n_raciones,$donacion,$cantidad_d,$anotacion){    
 
         global $bd;
-        $sql = "INSERT INTO menu (id_usuario,id_oferta,n_raciones,donacion,cantidad_d,anotacion) 
+        $sql = "INSERT INTO demanda (id_usuario,id_oferta,n_raciones,donacion,cantidad_d,anotacion) 
         VALUES ('$id_usuario','$id_oferta','$n_raciones','$donacion','$cantidad_d','$anotacion')";
         $bd->insertar($sql);
 
