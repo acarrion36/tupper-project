c4a310010d9e174fca7387815ebbca0fda7185de
a.carrion <101635337+acarrion36@users.noreply.github.com>
2022-11-24 12:07:49 +0100

    "demanda"

---
+++ php/api/api_demanda.php
@@ -0,0 +1,44 @@
+<?php
+// send some CORS headers so the API can be called from anywhere
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+      
+
+include_once("../class/class_demanda.php"); //incluimos clase producto
+include_once("../servicio.php"); 
+login();
+
+$_POST=json_decode(file_get_contents('php://input'),true); //decodificar el json
+
+
+switch($_SERVER['REQUEST_METHOD']){
+    case 'POST':
+            if (isset($_POST['id_usuario'])&&isset($_POST['id_oferta'])&&isset($_POST['n_raciones'])&&isset($_POST['donacion'])){
+                demanda::guardarDemanda($_POST['id_usuario'],$_POST['id_oferta'],$_POST['n_raciones'],$_POST['donacion'],$_POST['cantidad_d'],$_POST['anotacion']);
+            }else{
+                echo 'faltan datos basicos';
+            }
+         break;
+    case 'GET':  
+          if (isset($_GET['idd'])){
+               demanda::obtenerDemanda($_GET['idd']);  
+          }else{
+               demanda::obtenerDemanda(0);
+          }
+            
+         break;
+
+    case 'PUT':
+        
+         break;
+
+    case 'DELETE':
+          if (isset($_GET['id'])){
+               demanda::eliminarDemanda($_GET['id']);    
+          }
+         break;
+    }
+?>
---
+++ php/class/class_demanda.php
@@ -0,0 +1,72 @@
+<?php
+include("conexion.php");
+$bd = new BaseDatos();
+//------HOSTING ONLINE-----------
+$conexion= $bd->conectar("u116520402_elikawaste");
+//------LOCAL HOST-----------
+//$conexion= $bd->conectar("elika_waste");
+
+class demanda{
+
+    private $id_demanda;
+    private $id_usuario;
+    private $id_oferta;
+    private $n_raciones;
+    private $donacion;
+    private $cantidad_d;
+    private $anotacion;
+
+    public function __construct($id_demanda,$id_usuario,$id_oferta,$n_raciones,$donacion,$cantidad_d,$anotacion){
+
+        $this->id_demanda=$id_demanda;
+        $this->id_usuario=$id_usuario;
+        $this->id_oferta=$id_oferta;
+        $this->n_raciones=$n_raciones;
+        $this->donacion=$donacion;
+        $this->cantidad_d=$cantidad_d;
+        $this->anotacion=$anotacion;
+
+    }
+
+    public static function guardarMenu($id_usuario,$id_oferta,$n_raciones,$donacion,$cantidad_d,$anotacion){    
+
+        global $bd;
+        $sql = "INSERT INTO menu (id_usuario,id_oferta,n_raciones,donacion,cantidad_d,anotacion) 
+        VALUES ('$id_usuario','$id_oferta','$n_raciones','$donacion','$cantidad_d','$anotacion')";
+        $bd->insertar($sql);
+
+    }
+    public static function obtenerDemanda($idd){
+        global $bd;
+        $data=[];
+        if($idd==0){ 
+            $sql="SELECT * FROM demanda";
+            $resultado=$bd->seleccionar($sql);
+                while ($ofer = mysqli_fetch_assoc($resultado)) {
+                    $data[]=$ofer;
+                }
+            $var= json_encode($data);
+
+            echo $var;
+        }else{
+                $sql="SELECT * FROM demanda WHERE id_demanda='".$idd."'";
+                $resultado=$bd->seleccionar($sql);
+                while ($dem = mysqli_fetch_assoc($resultado)) {
+                        $data[]=$dem;
+                    }
+                $var= json_encode($data);
+                echo $var;
+            }
+        }
+        public static function eliminarDemanda($id){
+            global $bd;
+            $sql=" SET FOREIGN_KEY_CHECKS = 0";
+            $bd->eliminar($sql);
+            $sql="DELETE FROM demanda where id_demanda='$id'";
+            $bd->eliminar($sql);
+            $sql=" SET FOREIGN_KEY_CHECKS = 1";
+            $bd->eliminar($sql);
+        }
+    
+
+}
